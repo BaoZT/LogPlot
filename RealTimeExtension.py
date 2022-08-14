44f53b19614600900fff360d18cdc6d085a7f3de
BaoZT <baozhengtang13@163.com>
2022-08-14 13:51:17 +0800

    非跨线程发送对象引用的方式解决MVB槽函数偶尔收不到数据的情况

--- RealTimeExtension.py
+++ RealTimeExtension.py
@@ -1,3 +1,4 @@
+import copy
 import os
 import queue
 import re
@@ -10,7 +11,7 @@ from PyQt5 import QtCore
 from FileProcess import FileProcess
 from MainWinDisplay import InerIoInfo, InerIoInfoParse, InerRunningPlanInfo, InerRunningPlanParse, InerSduInfo, InerSduInfoParse
 from MsgParse import Atp2atoParse
-from TcmsParse import MVBParse
+from TcmsParse import Ato2TcmsCtrl, Ato2TcmsState, MVBParse, Tcms2AtoState
 from ConfigInfo import ConfigFile
 
 exit_flag = 0
@@ -18,8 +19,8 @@ queueLock = threading.Lock()
 workQueue = queue.Queue(10000)
 real_curve_buff = 50                                 # 每次绘图话添加的点数
 real_curve_all_buff = 50000                          # 每次绘画总点数
-newPaintList = np.zeros([5, real_curve_buff])        # 新添加的参数
-paintList = np.zeros([5, real_curve_all_buff])       # 初始化1000个点用于绘图
+newPaintList = np.zeros([5, real_curve_buff],dtype=int)        # 新添加的参数
+paintList = np.zeros([5, real_curve_all_buff],dtype=int)       # 初始化1000个点用于绘图
 
 class SerialRead(threading.Thread, QtCore.QObject):
 
@@ -32,13 +33,13 @@ class SerialRead(threading.Thread, QtCore.QObject):
     # 串口成功打开才启动此线程
     def run(self):
         # 读取文件，测试时打开
-        with open('M_L-Serial-COM6-1126170040-序列3-黑山北-阜新.log') as f:
+        with open('M_L-Serial-COM6-1126143651-序列1-北票-朝阳.log') as f:
             while not exit_flag:
                 # 有数据就读取
                 try:
                     #下面两行测试时打开
                     line = f.readline().rstrip()
-                    time.sleep(0.0001)
+                    time.sleep(0.001)
                     # line = self.ser.readline().decode('ansi', errors='ignore').rstrip()  # 串口设置，测试时注释
                 except UnicodeDecodeError as err:
                     print("serial read err")
@@ -59,6 +60,7 @@ class RealPaintWrite(threading.Thread, QtCore.QObject):
     planShowSignal = QtCore.pyqtSignal(InerRunningPlanInfo, int)
     ioShowSignal = QtCore.pyqtSignal(str,str,InerIoInfo)
     sduShowSignal = QtCore.pyqtSignal(InerSduInfo)
+    mvbShowSignal = QtCore.pyqtSignal(Ato2TcmsCtrl, Ato2TcmsState, Tcms2AtoState)
 
     def __init__(self, filepath=str, filenamefmt=str, portname=str):
         threading.Thread.__init__(self, )
@@ -84,6 +86,14 @@ class RealPaintWrite(threading.Thread, QtCore.QObject):
         self.rpParser = InerRunningPlanParse()
         self.mvbParser = MVBParse()
         self.atp2atoParser = Atp2atoParse()
+        # 解析结果
+        self.a2tCtrlObj  = None
+        self.a2tStatObj  = None
+        self.t2aStatObj  = None
+        self.atpatoMsgObj= None
+        self.sduInfoObj  = None
+        self.rpInfoObj   = None
+        self.ioInfoObj   = None
         # 静态变量
         self.cycleNumStr = '0'
         self.count = 0
@@ -104,7 +114,7 @@ class RealPaintWrite(threading.Thread, QtCore.QObject):
         self.sdu_ato = ()
         self.sdu_atp = ()
         self.state_machine = 0  # 测速测距检查使用的状态机用于匹配ATP/ATO速度
-    
+
     def newCyclePreProcess(self):
         # 创建周期后应重置ATP/ATO解析模块解析结果
         # 文件中可能有多个ATP/ATO消息时需要均解析，直到所有P->以及O->P解析完成后周期结束才能重置
@@ -186,7 +196,12 @@ class RealPaintWrite(threading.Thread, QtCore.QObject):
 
         # 匹配速度曲线信息
         if self.patCycleCommonResearch(line):
-            return    
+            return  
+
+        # 匹配MVB信息
+        if self.patMvbResearch(line):
+            return
+        
         # 匹配计划使用独立信号
         if self.patPlanResearch(line, self.cycle_os_time_start): 
             return
@@ -207,7 +222,6 @@ class RealPaintWrite(threading.Thread, QtCore.QObject):
             self.cycle_os_time_start = int(match[0][0])  # 当周期系统开始时间
             # 新的周期时重置之前的变量
             self.newCyclePreProcess()
-            update_flag = True
         else:
             match = self.cfg.reg_config.pat_cycle_end.findall(line)
             if match:
@@ -218,46 +232,36 @@ class RealPaintWrite(threading.Thread, QtCore.QObject):
                 match = self.cfg.reg_config.pat_time.findall(line)
                 if match:
                     self.timeContentStr = match[0]  # time 信息直接返回
-                    update_flag = True
                 else:
                     match = self.cfg.reg_config.pat_fsm.findall(line)
                     if match:
                         self.fsm = match[0]
-                        update_flag = True
                     else:
                         match = self.cfg.reg_config.pat_ctrl.findall(line)
                         if match:
                             self.sc_ctrl = match[0]
                             # 允许速度处理
                             self.sc_ctrl = self.sc_ctrl[:4] + (str(self.atp2atoParser.msg_obj.sp2_obj.v_permitted),) + self.sc_ctrl[4:]  # tuple拼接
-                            update_flag = True
                         else:
                             match = self.cfg.reg_config.pat_p2o.findall(line)
                             if match:
                                 self.atp2atoParser.msgParse(match[0])
-                                update_flag = True
                             else:
                                 match = self.cfg.reg_config.pat_o2p.findall(line)
                                 if match:
                                     self.atp2atoParser.msgParse(match[0])
-                                    update_flag = True
                                 else:
                                     match = self.cfg.reg_config.pat_stoppoint.findall(line)
                                     if match:
                                         self.stoppoint = match[0]
-                                        update_flag = True
-                                    elif self.patMvbResearch(line):
-                                        update_flag = True
                                     else:
                                         pass
         # 发射信号
         if update_flag == True:
             # 生成信号结果
             result = (self.cycleNumStr, self.timeContentStr, self.fsm, self.sc_ctrl, self.stoppoint,
-                  self.mvbParser.ato2tcms_ctrl_obj, self.mvbParser.ato2tcms_state_obj, 
-                  self.mvbParser.tcms2ato_state_obj,self.atp2atoParser.msg_obj, self.time_statictics)
-            self.patShowSignal.emit(result)
-
+                    self.atp2atoParser.msg_obj,self.mvbParser.tcms2ato_state_obj, self.time_statictics)
+            self.patShowSignal.emit(copy.deepcopy(result))
         # 返回用于画图
         return update_flag
 
@@ -282,11 +286,13 @@ class RealPaintWrite(threading.Thread, QtCore.QObject):
     # 测速测距识别
     def patSduInfoResearch(self, line=str, cycleTime=int):
         updateflag = False
-        self.sduInfoParser.sduInfoStringParse(line, cycleTime)
+        if 'v&p' in line:
+            self.sduInfoParser.sduInfoStringParse(line, cycleTime)
+            updateflag = self.sduInfoParser.sduInfo.updateflag
         # 收集到sdu_ato和sdu_atp
-        if self.sduInfoParser.sduInfo.updateflag:
-            self.sduShowSignal.emit(self.sduInfoParser.sduInfo)
-            updateflag = True
+        if updateflag:
+            self.ioInfoObj = copy.deepcopy(self.sduInfoParser.sduInfo)
+            self.sduShowSignal.emit(self.ioInfoObj)
         else:
             pass
         return updateflag
@@ -297,7 +303,8 @@ class RealPaintWrite(threading.Thread, QtCore.QObject):
         self.ioInfoParser.ioStringParse(line)
         # 发送信号
         if self.ioInfoParser.ioInfo.updateflagIn == True or self.ioInfoParser.ioInfo.updateflagOut:
-            self.ioShowSignal.emit(self.cycleNumStr, self.timeContentStr, self.ioInfoParser.ioInfo)
+            self.ioInfoObj =  copy.deepcopy(self.ioInfoParser.ioInfo)
+            self.ioShowSignal.emit(self.cycleNumStr, self.timeContentStr, self.ioInfoObj)
             updateflag = True
         else:
             pass
@@ -308,25 +315,34 @@ class RealPaintWrite(threading.Thread, QtCore.QObject):
         updateflag = False
         # 前提条件
         if 'MVB[' in line:
-            self.mvbParser.parseProtocol(line)
-            if self.mvbParser.ato2tcms_ctrl_obj.updateflag or \
-                self.mvbParser.ato2tcms_state_obj.updateflag or \
+            updateflag = True
+            [a2tCtrl, a2tStat, t2aStat ] = self.mvbParser.parseProtocol(line)
+
+            if self.mvbParser.ato2tcms_ctrl_obj.updateflag and \
+                self.mvbParser.ato2tcms_state_obj.updateflag and \
                 self.mvbParser.tcms2ato_state_obj.updateflag:
-                updateflag = True
+
+                self.a2tCtrlObj = copy.deepcopy(a2tCtrl)
+                self.a2tStatObj = copy.deepcopy(a2tStat)
+                self.t2aStatObj = copy.deepcopy(t2aStat)
+                self.mvbShowSignal.emit(self.a2tCtrlObj,self.a2tStatObj, self.t2aStatObj)
         return updateflag
 
     # 提取计划内容
     def patPlanResearch(self, line=str, osTime=int):
+        updateflag = False
         # 提高解析效率,当均更新时才发送信号
         if '[RP' in line:
             self.rpParser.rpStringParse(line, osTime)
+            updateflag = self.rpParser.rpInfo.updateflag
         # 发送信号
-        if self.rpParser.rpInfo.updateflag:
-            self.planShowSignal.emit(self.rpParser.rpInfo, osTime)
+        if updateflag:
+            self.rpInfoObj = copy.deepcopy(self.rpParser.rpInfo)
+            self.planShowSignal.emit(self.rpInfoObj, osTime)
         else:
             pass
         # 返回用于指示解析结果
-        return self.rpParser.rpInfo.updateflag
+        return updateflag
 
     # 统计ATO周期的运行时间, None不更新
     def staticticsCycleTime(self, cycle_num):
