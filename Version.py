e2d346556f8d8157fc8800361fce1a7eaa1d109d
BaoZT <baozhengtang13@163.com>
2022-08-29 17:32:25 +0800

    版本信息

---
+++ Version.py
@@ -0,0 +1,45 @@
+'''
+Author: Zhengtang Bao
+Contact: baozhengtang@crscd.com.cn
+File: 
+Date: 2022-08-20 10:37:01
+Desc: 
+LastEditors: Zhengtang Bao
+LastEditTime: 2022-08-20 11:31:15
+'''
+#!/usr/bin/env python
+# encoding: utf-8
+
+import random
+
+
+class VersionInfo(object):
+
+    def __init__(self) -> None:
+        self.major  = 4  # 主版本-软件进行了大量重写,这些重写使得无法实现向后兼容性。
+        self.junior = 0  # 子版本-新功能新版本照顾到了兼容性
+        self.build  = 0  # 编译版本-区分平台
+        self.revision = random.randint(1,99999)  # 修补版本-内部程序集可以互换
+        self.name = 'LogPlot'
+        self.note = 'alpha'
+    
+    def __getVerStr(self):
+        return str(self.major)+'.'+str(self.junior)+'.'+str(self.build)+'.'+str(self.revision)+'-'+self.note
+    
+    def getVerToken(self):
+        return self.name + '-Ver ' + self.__getVerStr()
+
+    def getVerDescription(self):
+        desc = \
+        "1.重写了数据处理,使用数据解析实现ATP/MVB信息\n"+\
+        "2.重写了显示处理逻辑统一了离线和在线数据控件\n"+\
+        "3.提供了软件配置功能"
+        return desc
+    
+    def getLicenseDescription(self):
+        desc = \
+        "Author  :Baozhengtang\n"+\
+        "License :(C) Copyright 2017-2022, Author Limited.\n"+\
+        "Contact :baozhengtang@crscd.com"
+        return desc
+    
