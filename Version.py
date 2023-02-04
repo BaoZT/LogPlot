bc32daef1f4d31d2faf42607bf7e9ffc57f18aab
BaoZT <baozhengtang13@163.com>
2023-02-04 10:42:22 +0800

    更新版本信息

--- Version.py
+++ Version.py
@@ -5,7 +5,7 @@ File:
 Date: 2022-08-20 10:37:01
 Desc: 
 LastEditors: Zhengtang Bao
-LastEditTime: 2022-11-18 15:17:54
+LastEditTime: 2023-01-10 11:44:48
 '''
 #!/usr/bin/env python
 # encoding: utf-8
@@ -17,11 +17,11 @@ class VersionInfo(object):
 
     def __init__(self) -> None:
         self.major  = 4  # 主版本-软件进行了大量重写,这些重写使得无法实现向后兼容性。
-        self.junior = 0  # 子版本-新功能新版本照顾到了兼容性
+        self.junior = 1  # 子版本-新功能新版本照顾到了兼容性
         self.build  = 0  # 编译版本-区分平台
-        self.revision = 221118  # 修补版本-内部程序集可以互换
+        self.revision = 230110  # 修补版本-内部程序集可以互换
         self.name = 'LogPlot'
-        self.note = 'Beta'
+        self.note = 'Alpha'
     
     def __getVerStr(self):
         return str(self.major)+'.'+str(self.junior)+'.'+str(self.build)+'.'+str(self.revision)+'-'+self.note
@@ -31,12 +31,9 @@ class VersionInfo(object):
 
     def getVerDescription(self):
         desc = \
-        "1.重写了数据处理,使用数据解析实现ATP/MVB信息\n"+\
+        "1.重写了数据处理,使用数据解析实现ATP/MVB/TSRS信息\n"+\
         "2.重写了显示处理逻辑统一了离线和在线数据控件\n"+\
-        "3.提供了软件配置功能\n"+\
-        "2022/11/15 修复SP134纯文本显示缺陷"
-        "2022/11/17 修复计划显示错误并补充信息"
-        "2022/11/18 增加等级模式,修复折返计划错误"
+        "3.提供了软件配置、协议解析、统计功能\n"
         return desc
     
     def getLicenseDescription(self):
